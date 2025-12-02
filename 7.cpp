4583fdf38371e8fd305cfe9f5921f6d0711ca4ad
24f1001670 <24f1001670@ds.study.iitm.ac.in>
2025-12-02 21:12:07 +0530

    updatee

---
+++ 7.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void leftRotate(vector<int>& arr, int d) {
+        int n = arr.size();
+        d = d % n;
+
+        reverse(arr.begin(), arr.begin() + d);  // Reverse first d
+        reverse(arr.begin() + d, arr.end());    // Reverse remaining
+        reverse(arr.begin(), arr.end());        // Reverse whole array
+    }
+};
