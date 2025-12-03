16f8ad4c2496f65d42e4a5812816830952e89ced
24f1001670 <24f1001670@ds.study.iitm.ac.in>
2025-12-03 18:24:18 +0530

    push

---
+++ 10.cpp
@@ -0,0 +1,20 @@
+//Maximum consecutive ones in a binary array
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt=0;
+        int maxi=0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                cnt++;
+                maxi= max(maxi,cnt); 
+            }else{
+                cnt=0;
+            }
+        }
+
+        return maxi;
+    }
+};
