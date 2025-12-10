90c3514f2a05d6d5647399e31942771b7454f140
aki030 <singhanik5593@gmail.com>
2025-12-10 23:07:47 +0530

    practice

---
+++ 22.cpp
@@ -0,0 +1,20 @@
+//Leaders in an array
+
+class Solution {
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans;
+        
+        int mx = INT_MIN;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] >= mx) {
+                ans.push_back(arr[i]);
+                mx = arr[i];
+            }
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
